598d0dd70a6111e7f5c0863b179d95318094f647
juvallejom <juvallejom@unal.edu.co>
2025-12-09 15:48:24 +0100

    [ADD] apc: Ex 1

---
+++ Algorithms and Parallel Computing/Labs/Ex 1/README.md
@@ -0,0 +1,3 @@
+The aim of this code is that the user can write the cost of a product for differentes stores. In this way, i want to separe and compute all the prices regions (Areas).
+
+There is a new gaol and is create or adapt the same code for a n number of regions or areas (In this case we're talking about 20 neightboors ) See pdf for instructions 
---
+++ Algorithms and Parallel Computing/Labs/Ex 1/build/CMakeFiles/PriceDeviationV1Class.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PriceDeviationV1Class.dir/main.cpp.o"
+  "CMakeFiles/PriceDeviationV1Class.dir/main.cpp.o.d"
+  "PriceDeviationV1Class"
+  "PriceDeviationV1Class.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PriceDeviationV1Class.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
